9713e83fb1055175c67a9be37ebeb02f881fa2b2
KUNYU47 <kwanjyu.li@mail.utoronto.ca>
2024-09-09 14:51:37 -0400

    refactored to extract helper method

---
+++ src/Multiples.java
@@ -0,0 +1,22 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        for (int i = 1; i <= 1000; i++) {
+
+            boolean divisableBy3 = i % 3 == 0;
+            boolean divisableBy5 = i % 5 == 0;
+
+            if (divisableBy3 || divisableBy5) {
+
+                count++;
+
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
